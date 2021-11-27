f6de9bd856b922ffd3db2250613de972f76c59c7
张政煜 <2647400883@qq.com>
2021-11-27 22:59:02 +0800

    项目架构提交

---
+++ gulimall-prod/src/test/java/com/atguigu/gulimail/prod/GulimallProdApplicationTests.java
@@ -0,0 +1,13 @@
+package com.atguigu.gulimail.prod;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GulimallProdApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
